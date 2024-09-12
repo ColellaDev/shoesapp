a2842d6cd1731d4773b2c3473f0b8dad7de6c920
Marcos Colella <colella.dev@gmail.com>
2024-09-12 16:05:09 -0300

    Utilizando tags para identificar usuário

--- App.tsx
+++ App.tsx
@@ -10,12 +10,15 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserEmailCreate } from './src/notifications/notificationsTags';
 
 OneSignal.initialize("6b6701e1-fc55-4acb-9e57-3cbd63e46adf")
 OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate("marcos.colella@gmail.com")
 
   return (
     <NativeBaseProvider theme={THEME}>
