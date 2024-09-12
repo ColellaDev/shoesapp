f49b12bb4601fd9230a2d30f801b8bb6727e048a
Marcos Colella <colella.dev@gmail.com>
2024-09-12 17:07:18 -0300

    Deixando mensagem dinâmica com tags

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
-import { tagUserEmailCreate } from './src/notifications/notificationsTags';
+import { tagUserInfoCreate } from './src/notifications/notificationsTags';
 
 OneSignal.initialize("6b6701e1-fc55-4acb-9e57-3cbd63e46adf")
 OneSignal.Notifications.requestPermission(true)
@@ -18,7 +18,7 @@ OneSignal.Notifications.requestPermission(true)
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  tagUserEmailCreate("marcos.colella@gmail.com")
+  tagUserInfoCreate()
 
   return (
     <NativeBaseProvider theme={THEME}>
