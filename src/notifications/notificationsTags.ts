ae801ac4df58e22b76d722277dcde9b6eb5a4cca
Marcos Colella <colella.dev@gmail.com>
2024-09-13 13:52:52 -0300

    Criando tag para carrinho

--- src/notifications/notificationsTags.ts
+++ src/notifications/notificationsTags.ts
@@ -6,3 +6,7 @@ export function tagUserInfoCreate() {
         user_email: "marcos.colella@gmail.com"
     })
 }
+
+export function tagCartUpdate(itemsCount: string) {
+    OneSignal.User.addTag('cart_items_count', itemsCount)
+}
